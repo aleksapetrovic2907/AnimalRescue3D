942cbfc1ff77e01c52ea53b6c9a8ce3230c3bc93
aleksapetrovic2907 <aleksa.petrovic2907@gmail.com>
2022-12-07 20:57:00 +0100

    Setup dogs

--- Assets/_Game/Scripts/FeedbackManager.cs
+++ Assets/_Game/Scripts/FeedbackManager.cs
@@ -4,9 +4,12 @@ using Aezakmi.Animals;
 
 namespace Aezakmi
 {
-    public class FeedbackManager : MonoBehaviour
+    public class FeedbackManager : GloballyAccessibleBase<FeedbackManager>
     {
         [SerializeField] private GameObject animalCaptured_VFX;
+
+        [SerializeField] private AudioClip upgrade_SFX;
+        [SerializeField] private AudioSource audioSource;
 
         private void OnEnable()
         {
@@ -23,6 +26,11 @@ namespace Aezakmi
             var animal = (Transform)message["animal"];
             var topOfHead = animal.GetComponent<AnimalController>().topOfHead;
             Instantiate(animalCaptured_VFX, topOfHead.position, Quaternion.identity, topOfHead);
+        }
+
+        public void Upgraded()
+        {
+            audioSource.PlayOneShot(upgrade_SFX);
         }
     }
 }
